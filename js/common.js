e7de899802ab78aa727a0550358730d48466c4c1
nasmakd <110151531+nasmakd@users.noreply.github.com>
2022-10-19 16:27:57 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -13,6 +13,8 @@ $('.menu_btn').click(function () {
 
 $(window).resize(function () {
    let winW = $(window).width();
+   let contentW1 = $('.full_name').width();
+   let contentW2 = $('.op_hour').width()
    console.log(winW)
    if (winW >= 1023 && !$('nav').is(':hidden')) {
       $('nav').removeAttr('style');
@@ -23,4 +25,20 @@ $(window).resize(function () {
    } else {
       $('body').css({ 'overflow': 'auto' });
    }
+   $('.request_sel, .msg').css({width: contentW1})
+   $('.shop').css({width: contentW2})
+
 });
+let contentW1 = $('.full_name').width();
+$('.request_sel, .msg').css({width: contentW1})
+let contentW2 = $('.op_hour').width();
+$('.shop').css({width: contentW2})
+
+$('#contact input, #contact select, #contact textarea').on("focus", function(){
+   $(this).parent('div').css({'border-color': '#AD8B73'})
+   $(this).siblings('label').css({'letter-spacing': '3px', 'font-weight': '700'})
+})
+$('#contact input, #contact select, #contact textarea').on("blur", function(){
+   $(this).parent('div').css({'border-color': 'transparent'})
+   $(this).siblings('label').css({'letter-spacing': '0', 'font-weight': '400'})
+})
